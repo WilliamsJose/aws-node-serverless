1dbf82a7215f9668b2bea0f89404aabc6c550cab
will <contatowilliamsj@gmail.com>
2024-04-05 15:26:32 -0300

    feat: aws retain var data for a short time
    in first tests they retain for less than 5 minutes, on second test the retention lasted for more than 5 minutes but less than 10

--- src/functions/getCustomers.js
+++ src/functions/getCustomers.js
@@ -1,30 +1,16 @@
 "use strict";
-const AWS = require("aws-sdk");
+
+const algumArray = [];
 
 module.exports.getCustomers = async (event) => {
-  const scanParams = {
-    TableName: process.env.DYNAMODB_CUSTOMER_TABLE,
-  };
+  console.log("Received event:", JSON.stringify(event, null, 2));
 
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-  const result = await dynamodb.scan(scanParams).promise();
+  const localDateTime = [
+    new Date().toDateString(),
+    new Date().toLocaleTimeString(),
+  ].join(" ");
 
-  if (result.Count === 0) {
-    return {
-      statusCode: 404,
-    };
-  }
+  algumArray.push(localDateTime);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      total: result.Count,
-      items: await result.Items.map((customer) => {
-        return {
-          name: customer.primary_key,
-          email: customer.email,
-        };
-      }),
-    }),
-  };
+  return algumArray;
 };
